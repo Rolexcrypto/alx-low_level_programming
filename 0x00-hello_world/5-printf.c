4a728de23b135c557b3af9ba0a9bf9cf91adf396
Roland <rolandekanem04@gmail.com>
2022-09-16 09:26:58 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+* this program prints 
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
