16a9a632334b0b484bcbc2fed4ae04f28347c639
Roland <rolandekanem04@gmail.com>
2022-09-20 08:05:29 -0700

    task .0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,17 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the string in the put function
+ * 
+ * Description: using the main function
+ * this program prints "a random number and check whether it is positive, negative, or zero"
+ * Return: 0
+ * /
 int main(void)
 {
 		int n;
