bf38ba1e6620bc06f4a393f701a8e56977d18119
Rolexcrypto <rolandekanem04@gmail.com>
2022-09-21 18:27:09 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,13 @@
 
 int main(void)
 {
-	int n = 0;
+	int n = 48;
 
-	while (n <= 15)
+	while (n <= 102)
 	{
-		putchar(n + '0');
+		putchar(n);
+		if (n == 57)
+			n += 39;
 			++n;
 	}
 	putchar('\n');
