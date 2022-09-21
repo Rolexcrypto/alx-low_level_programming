fa0c9ef944bda29e617c54ef362c2992a9fd1537
Roland <rolandekanem04@gmail.com>
2022-09-21 08:26:06 -0700

    task .7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: this program prints "The lowercase alphabet in reverse"
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lower = 'z';
+	
+	while (lower <= 'a')
+	{
+		putchar(lower);
+		--lower;
+	}
+	putchar('\n');
+
+	return (0);
+}
