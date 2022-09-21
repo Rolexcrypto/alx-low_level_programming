d7693c9e9c25973ad2c4260a43ec46cc11929609
Roland <rolandekanem04@gmail.com>
2022-09-21 07:12:44 -0700

    task .4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "The alphabet in lowercase except q and e"
+* Return: 0
+*/
+
+int main(void)
+{
+	char lower = 'a';
+	
+	while (lower <= 'z')
+	{
+		if (lower == 'e' || lower == 'q')
+			++lower;
+		putchar(lower);
+		++lower;
+	}
+	putchar('\n');
+
+	return (0);
+}
