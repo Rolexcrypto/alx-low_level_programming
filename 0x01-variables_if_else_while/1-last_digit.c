02c3dbc25d051c0563d1d4e9b61528d8cae2221b
Rolexcrypto <rolandekanem04@gmail.com>
2022-09-21 13:49:15 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,14 +17,16 @@ int main(void)
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-					
-	 	digit = n % 10;
+		digit = n % 10;
 
-		if (digit > 5) {
+		if (digit > 5)
+		{
 			printf("Last digit of %d is %d and is greater than 5\n", n, digit);
-		} else if (digit == 0) {
+		} else if (digit == 0)
+		{
 			printf("Last digit of %d is %d and is 0\n", n, digit);
-		} else if (digit < 6 && digit != 0) {
+		} else if (digit < 6 && digit != 0)
+		{
 			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 		}
 
