b526671c543315eb36413088d14dcfae31963d51
Rolexcrypto <rolandekanem04@gmail.com>
2022-09-21 13:34:00 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,29 +3,29 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
- * Description: using the main function
- * this program prints
- * "A random number and check whether it is positive, negative, or zero"
- * Return: 0
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints
+* "A random number and check whether it is positive, negative, or zero"
+* Return: 0
+*/
 int main(void)
 {
-		int n;
-		int digit;
+	int n;
+	int digit;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
+		srand(time(0));
+			n = rand() - RAND_MAX / 2;
 					
-				digit = n % 10;
+			digit = n % 10;
 
-				if (digit > 5) {
-					printf("Last digit of %d is %d and is greater than 5\n", digit);
-				} else if (digit == 0) {
-					printf("Last digit of %d is %d and is 0\n", digit);
-				} else if (digit < 6 && digit != 0) {
-					printf("Last digit of %d is %d and is less than 6 and not 0\n, n, digit);
-				}
-					return (0);
+			if (digit > 5) {
+				printf("Last digit of %d is %d and is greater than 5\n", digit);
+			} else if (digit == 0) {
+				printf("Last digit of %d is %d and is 0\n", digit);
+			} else if (digit < 6 && digit != 0) {
+				printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+			}
+			return (0);
 }
