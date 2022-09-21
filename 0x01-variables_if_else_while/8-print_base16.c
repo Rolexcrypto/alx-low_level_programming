118317ff92fca681712c2fa61a3a95e12f6c697c
Roland <rolandekanem04@gmail.com>
2022-09-21 08:56:17 -0700

    task .8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "All the numbers of base 16 in lowercase"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 15)
+	{
+		putchar(n ='0');
+		++n;
+	}
+	putchar('\n');
+
+	return (0)
+}
