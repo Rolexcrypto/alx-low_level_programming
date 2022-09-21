fabc7af7ef8ad06b52b37d60f38c4ee2f12f980e
Roland <rolandekanem04@gmail.com>
2022-09-21 07:47:49 -0700

    task .6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - print the string in the put function
+*
+* description: using the main function
+* this program prints "All single numbers of base 10 starting from 0"
+* Return: 0
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n = '0');
+		++n;
+	}
+	putchar('\n');
+
+	return (0);
+}
