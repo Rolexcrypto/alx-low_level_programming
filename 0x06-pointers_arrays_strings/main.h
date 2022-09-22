eff92fcf433661a5463be8b7973d787c0ad5864a
Rolexcrypto <rolandekanem04@gmail.com>
2022-09-22 17:28:11 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,22 +1,21 @@
 #ifndef MAIN_H
-	#define MAIN_H
-	#include <stdio.h>
+#define MAIN_H
+#include <stdio.h>
 
-	/**
-	 * void prototypes()
-	 * int prototypes()
-	 * char prototypes()
-	 */
-
-	void reverse_array(int *a, int n);
-	int _strcmp(char *s1, char *s2);
-	char *string_toupper(char *);
-	char *cap_string(char *);
-	char *_strcat(char *dest, char *src);
-	char *_strncpy(char *dest, char *src, int n);
-	char *_strncat(char *dest, char *src, int n);
-	char *leet(char *);
-
+/**
+* void prototypes()
+* int prototypes()
+* char prototypes()
+*/
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *leet(char *);
 
 
-	#endif /*MAIN_H*/
+
+#endif /*MAIN_H*/
