e65880a4b19612402e651af19d3cd95cb9e5324e
Rolexcrypto <rolandekanem04@gmail.com>
2022-09-23 01:27:56 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,7 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (1 = 0; 1 < n && src[i] != '\0'; i++)
+	for (1 = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
 	for ( ; i < n; i++)
