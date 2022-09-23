acdd7d49e94d256a3aef8b140fa1b48fc4df932c
Rolexcrypto <rolandekanem04@gmail.com>
2022-09-23 01:49:17 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*_strncpy - C function that copies a string, including the
+* _strncpy - C function that copies a string, including the
 *terminating null byte, using at most an inputted number of bytes.
 *if the length of the source string is less than the maximum byte number,
 *the remainder of the destination string is filled with null bytes.
@@ -12,15 +13,38 @@
 *Return: returns
 */
 
-char *_strncpy(char *dest, char *src, int n)
+
+
+int main(void)
 {
-	int i;
+    char s1[98];
+    char *ptr;
+    int i;
 
-	for (1 = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
-	return (dest);
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
+    printf("%s\n", s1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+    printf("%s\n", s1);
+    printf("%s\n", ptr);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+    printf("%s", s1);
+    printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
+    return (0);
 }
