231b71c5f62ebfc1bc7e8616ba41c7f992f12328
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-10 09:20:04 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,10 +17,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of char: %i byte(s)\n", sizeof(c));
-	printf("Size of int: %i byte(s)\n", sizeof(i));
-	printf("Size of long int: %i byte(s)\n", sizeof(li));
-	printf("Size of long long int: %i byte(s)\n", sizeof(lli));
-	printf("Size of float: %i byte(s)\n", sizeof(f));
+	printf("Size of a char: %i byte(s)\n", sizeof(c));
+	printf("Size of an int: %i byte(s)\n", sizeof(i));
+	printf("Size of a long int: %i byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(lli));
+	printf("Size of a float: %i byte(s)\n", sizeof(f));
 	return (0);
 }
