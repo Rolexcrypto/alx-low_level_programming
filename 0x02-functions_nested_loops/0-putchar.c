75846bae7d208892c25019a4a95a6bfebbcf9b2f
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-14 17:09:13 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,6 +4,8 @@
 /**
  * main - starting point
  *
+ * Description: using the main function
+ * this program print "a program that prints _putchar, followed by a new line"
  * return: 0
  */
 int main(void)
