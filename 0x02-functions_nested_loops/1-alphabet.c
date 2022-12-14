3d276129f436757bd3e34deb11e9df8638941459
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-14 18:05:38 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* print_alphabet - print alaphabet
+*
+* Description: using the main function
+* this program print "a function that prints the alphabet, in lowercase, followed by a new line"
+* Return: 0
+*/
+
+int main(void)
+{
+
+	int n;
+	
+	for (n = 'a'; n<= 'z'; n++)
+	{
+		putchar('n');
+	}
+	putchar('\n');
+	return (0);
+}
+
