04aeb75f1210263e7bbb7bb914d3ce8162c410ab
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-14 23:12:25 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@
 * print_alphabet - print alphabet from the put function
 *
 * Description: using the print_alphabet function
-* this program print 
+* this program print
 * "a function that prints the alphabet in lowercase, followed by a new line"
 * Return: 0
 */
@@ -15,10 +15,10 @@
 void print_alphabet(void)
 {
 
-	char letter;
+	char le;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-				_putchar('letter');
+	for (le = 'a'; l <= 'z'; le++)
+				_putchar('le');
 
 	_putchar('\n');
 }
