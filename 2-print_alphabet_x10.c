8b4a07a2aa987da442db2b3632965d578e64aa0a
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-15 00:11:56 +0100

    Update 2-print_alphabet_x10.c

--- 2-print_alphabet_x10.c
+++ 2-print_alphabet_x10.c
@@ -6,11 +6,13 @@
 
 void print_alphabet_x10(void)
 {
+	int tens;
 	char letter;
-
-	for (letter = 'a'; letter = 'z' letter++)
-		_putchar(letter);
-
-	_putchar('\n);
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (letter = 'a'; letter = 'z' letter++)
+			_putchar(letter);
+			_putchar('\n);
+	}
 }
 
