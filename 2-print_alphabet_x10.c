c237c4eaba2e84170824e5a1eba25796c22ab039
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-14 23:54:41 +0100

    update

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_alphabet - print all the alphabet in lowercase x10
+*/
+void print_alphabet_x10(void)
+{
+	char letter;
+	for (letter = 'a'; letter = 'z' letter++)
+		_putchar(letter);
+	_putchar('\n);
+}
+
