726dbcd39e6452adb5da8cf0060c015c7146a5f7
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-14 23:31:17 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,19 +5,15 @@
 
 /**
 * print_alphabet - print all alphabet in lowercase
-*
-* Description: using the print_alphabet function
-* this program print
-* "a function that prints the alphabet in lowercase, followed by a new line"
 */
 
 void print_alphabet(void)
 {
 
-	char l;
+	char letter;
 
-	for (l = 'a'; l <= 'z'; l++)
-		putchar('l');
+	for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar('letter');
 
-	putchar('\n');
+	_putchar('\n');
 }
