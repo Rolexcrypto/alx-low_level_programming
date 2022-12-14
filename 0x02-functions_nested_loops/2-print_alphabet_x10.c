e4b0dc1476699671edeb244ef4ab6a53eaa9fe5f
Rolexcrypto <111367658+Rolexcrypto@users.noreply.github.com>
2022-12-15 00:50:44 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	char letter;
 	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (letter = 'a'; letter = 'z' letter++)
+		for (letter = 'a'; letter = 'z'; letter++)
 			_putchar(letter);
-			_putchar('\n);
+			_putchar('\n');
 	}
 }
 
