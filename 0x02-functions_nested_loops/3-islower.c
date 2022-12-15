c27d49f122c6d1321710d78fbf4c412a79cc89cd
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-15 08:19:01 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
 * _islower(int c) - checks if the character is lowercase
-*
 * @: a character argument
-* this program check "if the character is lowercase"
 * Return: returns 1 and 0 depending on conditions
 */
 
 int _islower(int c)
 {
+
 	return (c >= 'a' && c <= 'z');
+
 }
