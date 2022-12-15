52ca09ccd3510feb32e6d5dc6730955a8c337bb1
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-15 08:02:26 +0100

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _islower - checks if the character is lowercase
+*
+* Description: using the int _islower(int c)
+* this program check "if the character is lowercase"
+* Return: returns 1 and 0 depending on conditions
+*/
+
+int _islower(int c)
+{
+	return (c=> 'a' && c <= 'z');
+}
+
