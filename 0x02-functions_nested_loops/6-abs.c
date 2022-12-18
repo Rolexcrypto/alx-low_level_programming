aed1367642f2e4a2e3748b487393ad0ea0663f09
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-18 08:25:26 +0100

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute value of an inter
+* @t: the integer to be computed
+* Return: the absolute value
+*/
+
+int _abs(int)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
+
