a767618be131281c7d24b165704543197c3ff247
Rolexcrypto <rolandekanem04@gmail.com>
2022-12-18 08:12:37 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
 * print_sign - prints the sign of a number
 * @n: the number of the sign to be printed
-* Return: 1 if number is greater than zero
-* 0 if number is zero
-* -1 if number is less than zero
+* Return: return 1 if number is greater than zero,
+* return 0 if number is zero and
+* return -1 if number is less than zero
 */
 
 int print_sign(int n)
